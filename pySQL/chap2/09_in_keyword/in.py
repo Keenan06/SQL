62f6dba3518fded05da4c9b0850c9006e57319f8
Keenan06 <78598120+Keenan06@users.noreply.github.com>
2021-05-12 12:33:14 +0200

    Delete pySQL directory

--- pySQL/chap2/09_in_keyword/in.py
+++
@@ -1,6 +0,0 @@
-friends = ["Rolf", "Bob", "Jen"]
-print("Jen" in friends)
-
-movies = {"Bird box", "Blair witch project", "Lilo and stitch"}
-user_movie = input("Enter a recent movie")
-print(user_movie in movies)
